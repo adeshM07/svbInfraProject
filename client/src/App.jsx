0ab5c588fa932fbfc2280d8f32d83f81bd8e78ca
adeshM07 <adesh.m@vedapixel.com>
2025-12-09 12:40:58 +0530

    solving google sub navigation issue phase 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import SRP from "./pages/SRP";
 
 // import lvideo from "./assets/landingVideo.mov";
 // import lvideo from './videos/LandingVideoTrimmed.mp4'
-import lvideo from './videos/LandingVideoTrimmed.mp4'
+import lvideo from "./videos/LandingVideoTrimmed.mp4";
 import "./App.css";
 
 const App = () => {
@@ -30,8 +30,14 @@ const App = () => {
 
       {isLandingPage && (
         <div className="hero-video-container  w-screen">
-          <video src="https://res.cloudinary.com/dsf0pohxi/video/upload/v1765171912/LandingVideoTrimmed_i8mru3.mp4" autoPlay loop muted playsInline />
-           {/* <video
+          <video
+            src="https://res.cloudinary.com/dsf0pohxi/video/upload/v1765171912/LandingVideoTrimmed_i8mru3.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+          {/* <video
   src="https://www.dropbox.com/scl/fi/zaa7la43gnisr3qhyuzto/landingVideo.MOV?rlkey=rj9jrqt1jifxyxvml8fcwmx8i&raw=1"
   autoPlay
   loop
@@ -43,11 +49,15 @@ const App = () => {
       )}
 
       {/* FIX → Add margin only for landing, top padding for other pages */}
-      <div className="w-screen" style={{ marginTop: isLandingPage ? "100vh" : "9vh" }} >
+      <div
+        className="w-screen"
+        style={{ marginTop: isLandingPage ? "100vh" : "9vh" }}
+      >
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/services" element={<Service />} />
+          <Route path="/excavators-dumpers-on-hire" element={<Service />} />
           <Route path="/our-fleet-2" element={<Fleet />} />
           <Route path="/portfolio" element={<PortFolio />} />
           <Route path="/gallery" element={<PortFolio />} />
@@ -57,6 +67,8 @@ const App = () => {
           <Route path="/rep" element={<REP />} />
           <Route path="/ds" element={<DS />} />
           <Route path="/srp" element={<SRP />} />
+          <Route path="/mass-earth-excavation-2" element={<ESE />} />
+          <Route path="/mass-rock-excavation" element={<REP />} />
         </Routes>
 
         <Footer />
