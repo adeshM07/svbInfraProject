b30601c70f4a3a2b17716512bc2f6fa59dee4735
adeshM07 <adesh.m@vedapixel.com>
2025-12-16 16:29:24 +0530

    whatsapp number updated

--- client/src/pages/WhatsAppFloat .jsx
+++ client/src/pages/WhatsAppFloat .jsx
@@ -1,22 +1,20 @@
 import React, { useEffect } from "react";
 
 const WhatsAppFloat = () => {
-  
-
   return (
-    
     <a
-      href="https://wa.me/919036354263" // replace with your number
+      href="https://wa.me/919036354261" // replace with your number
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-10 right-5 z-[9999]"
     >
       <div className="rounded-full flex items-center justify-center transition whatsapp-pulse">
-  <i className="fa-brands fa-whatsapp text-green-500
+        <i
+          className="fa-brands fa-whatsapp text-green-500
     [@media(min-width:300px)_and_(max-width:700px)]:text-[2.5rem]
-    text-[4rem]">
-  </i>
-</div>
+    text-[4rem]"
+        ></i>
+      </div>
     </a>
   );
 };
