a59ef5caed185dffc74a29f4960fd2fc85f4645e
adeshM07 <adesh.m@vedapixel.com>
2025-12-18 12:48:48 +0530

    reduce menu contat us button size

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -204,7 +204,7 @@ const Menu = () => {
 
             <Link to="/contact">
               <button
-                className="menuButton [@media(min-width:650px)_and_(max-width:1200px)]:h-[11vh] [@media(min-width:650px)_and_(max-width:1200px)]:w-[14vw] md:w-[15vw] md:h-[4vh] w-[9vw] h-[6vh] lg:h-[6vh] lg:w-[9vw] bg-[#FDC000] text-black text-[1rem] rounded-[9px] transition duration-300 transform
+                className="menuButton [@media(min-width:650px)_and_(max-width:1200px)]:h-[11vh] [@media(min-width:650px)_and_(max-width:1200px)]:w-[14vw] md:w-[15vw] md:h-[4vh] w-[9vw] h-[6vh] lg:h-[6vh] lg:w-[9vw] [@media(min-width:2500px)]:h-[5vh] bg-[#FDC000] text-black text-[1rem] rounded-[9px] [@media(min-width:2500px)]:text-[1.4rem] transition duration-300 transform
   hover:scale-105 hover:bg-[#ffcf33] cursor-pointer"
               >
                 Contact Us
