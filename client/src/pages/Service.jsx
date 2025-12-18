d9ba53c18b885d15f7ad1e75cac5a87c17933fef
adeshM07 <adesh.m@vedapixel.com>
2025-12-18 13:56:06 +0530

    reduce menu contat us button size

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -43,7 +43,7 @@ const Service = () => {
           <div className="pt-[80px] ml-[30px] [@media(min-width:300px)_and_(max-width:410px)]:pt-[60px] lg:pt-[120px] lg:ml-[120px]">
             {/* Heading Animation */}
             <motion.p
-              className="text-white text-[1.5rem] [@media(min-width:650px)_and_(max-width:1200px)]:text-[1.5rem] [@media(min-width:650px)_and_(max-width:1200px)]:leading-7 [@media(min-width:300px)_and_(max-width:410px)]:text-[1.2rem] [@media(min-width:300px)_and_(max-width:410px)]:leading-6  md:text-[3.2rem] lg:text-[4rem] aboutCom leading-8 md:leading-14 lg:leading-19"
+              className="text-white text-[1.5rem] [@media(min-width:650px)_and_(max-width:1200px)]:text-[1.5rem] [@media(min-width:650px)_and_(max-width:1200px)]:leading-7 [@media(min-width:300px)_and_(max-width:410px)]:text-[1.2rem] [@media(min-width:300px)_and_(max-width:410px)]:leading-6  md:text-[3.2rem] lg:text-[4rem] aboutCom leading-8 md:leading-14 lg:leading-19 [@media(min-width:2500px)]:text-[6rem] [@media(min-width:2500px)]:leading-24"
               initial={{ opacity: 0, y: 40 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, ease: "easeOut" }}
@@ -68,7 +68,7 @@ const Service = () => {
                   safely, and efficiently.
                 </p>
               ) : (
-                <p className="popins text-[10px] md:text-[1.2rem] lg:text-[20px] text-white mt-[18px]">
+                <p className="popins text-[10px] md:text-[1.2rem] lg:text-[20px] text-white mt-[18px] [@media(min-width:2500px)]:text-[2rem]">
                   Delivering top-quality machinery and equipment to ensure every
                   project runs smoothly, <br className="md:hidden lg:block" />
                   safely, and efficiently.
