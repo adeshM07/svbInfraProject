a65d1c07aefaeff268dc9a84fc088050df829ea9
adeshM07 <adesh.m@vedapixel.com>
2025-12-20 20:24:07 +0530

    reduced the size of the service card for 360px width mobile

--- client/src/pages/SRP.jsx
+++ client/src/pages/SRP.jsx
@@ -9,7 +9,7 @@ import ic2 from "../assets/SRPCardIMG2.png";
 import ic3 from "../assets/SRPCardIMG3.png";
 import ic4 from "../assets/SRPCardIMG4.png";
 import ic5 from "../assets/SRPCardIMG5.png";
-import ic6 from "../assets/SRPCardIMG6.png";
+import ic6 from "../assets/SRPCardIMG6.JPEG";
 import ic7 from "../assets/SRPCardIMG7.png";
 import ic8 from "../assets/SRPCardIMG8.JPEG";
 import fb from "../assets/forwardButton.png";
