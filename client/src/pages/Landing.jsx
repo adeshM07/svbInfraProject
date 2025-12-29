8146d5b319faf92cdd7f2b1add5dbe033c25d563
adeshM07 <adesh.m@vedapixel.com>
2025-12-29 14:05:10 +0530

    resizing the project section of the landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -486,7 +486,7 @@ const Landing = () => {
                   developments delivered with quality and precision.
                 </p>
               ) : (
-                <p className="text-[#4D4D4D] w-[48%] text-justify popins [@media(min-width:650px)_and_(max-width:1200px)]:text-[0.9rem]  text-[0.6rem] lg:text-[1.5rem] lg:mt-3 font-semibold ">
+                <p className="text-[#4D4D4D] w-[48%] text-justify popins [@media(min-width:650px)_and_(max-width:1200px)]:text-[0.9rem]  text-[0.6rem]  lg:text-[1.5rem] xl:text-[1.2rem] lg:mt-3 font-semibold ">
                   We have successfully completed a wide range of 
                   earthwork and excavation projects for leading  clients
                   across Karnataka and Andhra Pardesh.  Our portfolio includes  residential,
@@ -498,7 +498,7 @@ const Landing = () => {
               className="absolute overflow-hidden -top-4 w-full left-[65%]  md:left-[60%] md:-top-10  lg:left-[55%] lg:-top-24  xl:left-[55%] xl:-top-20"
               // className="absolute -top-4  md:-top-10 lg:-top-24 xl:-top-20"
               >
-                <div className="flex lg:gap-x-6 gap-x-2 animate-fleet-scroll ">
+                <div className="flex lg:gap-x-6 gap-x-2 animate-fleet-scroll">
                   {[
                     {
                       src: c1,
